baa782fbea037765ce0f1924fc554400dbc2281e
Александр Pistak <pistak@deeplay.io>
2024-04-06 01:55:33 +0700

    изменил /News

---
+++ NewWebApp/Controllers/GeneralNewsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewWebApp.Models;
+
+namespace NewWebApp.Controllers
+{
+	public class GeneralNewsController : Controller
+	{
+		private readonly NewsRepository NewsRepository;
+		public GeneralNewsController(NewsRepository NewsRepository)
+		{
+			this.NewsRepository = NewsRepository;
+		}
+		public ActionResult Index(Guid id)
+		{
+			var model = NewsRepository.GetArticles();
+
+			if (id != default)
+			{
+				return View("Show", NewsRepository.GetArticleById(id));
+			}
+
+			return View(model);
+		}
+	}
+}
